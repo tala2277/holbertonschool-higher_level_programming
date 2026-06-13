e1bc0cbf7c19019ce4b80c644e8e0cb3de1da836
tala2277 <tt1341700@gmail.com>
2026-06-13 09:02:30 +0000

    reset task 101

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module that multiplies two matrices using NumPy."""
+"""Module for lazy matrix multiplication."""
 
-import numpy
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
     """Multiply two matrices using NumPy."""
-    return numpy.matrix(m_a) * numpy.matrix(m_b)
+    return np.matmul(m_a, m_b)
