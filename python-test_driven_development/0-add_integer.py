9fa63c1db74c98a1d83c892dab84b25f5cdab53d
tala2277 <tt1341700@gmail.com>
2026-06-13 08:45:25 +0000

    fix edge cases

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -19,10 +19,11 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    if isinstance(a, float) and a != a:
-        raise TypeError("a must be an integer")
-
-    if isinstance(b, float) and b != b:
-        raise TypeError("b must be an integer")
-
-    return int(a) + int(b)
+    try:
+        return int(a) + int(b)
+    except (ValueError, OverflowError):
+        raise TypeError(
+            "a must be an integer"
+            if isinstance(a, float)
+            else "b must be an integer"
+        )
