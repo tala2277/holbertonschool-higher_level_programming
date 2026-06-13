efa0f7565ca25c7ce9788021d445a7a1c3f52805
tala2277 <tt1341700@gmail.com>
2026-06-13 06:17:30 +0000

    Task 13

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    return sum(score * weight for score, weight in my_list) / \
+        sum(weight for score, weight in my_list)
