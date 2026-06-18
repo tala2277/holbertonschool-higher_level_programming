8d400cee2c0f9f7dbe020765e9dafee95b9cce02
tala2277 <tt1341700@gmail.com>
2026-06-18 14:59:57 +0000

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize a square."""
+        self.__size = size
