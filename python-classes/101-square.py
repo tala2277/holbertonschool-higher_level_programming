74fc230514ffce9bed7873bbf1e6ce56b2b7b1c9
tala2277 <tt1341700@gmail.com>
2026-06-18 15:04:35 +0000

    Task 101

---
+++ python-classes/101-square.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""This module defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a square."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieve the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve the position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position."""
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(i, int) for i in value) or
+                value[0] < 0 or value[1] < 0):
+            raise TypeError(
+                "position must be a tuple of 2 positive integers"
+            )
+
+        self.__position = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square using #."""
+        if self.__size == 0:
+            print("")
+            return
+
+        print("\n" * self.__position[1], end="")
+
+        for i in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        """Return printable square."""
+        if self.__size == 0:
+            return ""
+
+        lines = []
+        lines.extend([""] * self.__position[1])
+
+        for i in range(self.__size):
+            lines.append(
+                " " * self.__position[0] + "#" * self.__size
+            )
+
+        return "\n".join(lines)
