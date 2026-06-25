f55c9b0134f72887fd50ac0c7fb8adb86130188a
tala2277 <tt1341700@gmail.com>
2026-06-25 20:10:01 +0000

    Task 2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""VerboseList module."""
+
+
+class VerboseList(list):
+    """List that prints notifications when modified."""
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, iterable):
+        count = len(iterable)
+        super().extend(iterable)
+        print(f"Extended the list with [{count}] items.")
+
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = self[index]
+        print(f"Popped [{item}] from the list.")
+        return super().pop(index)
