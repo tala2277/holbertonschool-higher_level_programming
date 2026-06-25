91a01a75b76fe5ac80d0a97c1fb1da691fdb948e
tala2277 <tt1341700@gmail.com>
2026-06-25 19:48:55 +0000

    Fix task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -6,53 +6,45 @@ from math import pi
 
 
 class Shape(ABC):
-    """Abstract base class for shapes."""
+    """Abstract shape."""
 
     @abstractmethod
     def area(self):
-        """Return the area of the shape."""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """Return the perimeter of the shape."""
         pass
 
 
 class Circle(Shape):
-    """Circle class."""
+    """Circle."""
 
     def __init__(self, radius):
-        """Initialize a circle."""
         self.radius = radius
 
     def area(self):
-        """Return the area."""
         return pi * (abs(self.radius) ** 2)
 
     def perimeter(self):
-        """Return the perimeter."""
-        return 2 * pi * abs(self.radius)
+        return abs(2 * pi * self.radius)
 
 
 class Rectangle(Shape):
-    """Rectangle class."""
+    """Rectangle."""
 
     def __init__(self, width, height):
-        """Initialize a rectangle."""
         self.width = width
         self.height = height
 
     def area(self):
-        """Return the area."""
         return abs(self.width * self.height)
 
     def perimeter(self):
-        """Return the perimeter."""
-        return 2 * (abs(self.width) + abs(self.height))
+        return abs(2 * (self.width + self.height))
 
 
 def shape_info(shape):
     """Print shape information."""
-    print(f"Area: {shape.area()}")
-    print(f"Perimeter: {shape.perimeter()}")
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
