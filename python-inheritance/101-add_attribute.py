bad6106936d70e14a2bd2286075437c00bb6fff4
tala2277 <tt1341700@gmail.com>
2026-06-25 19:21:18 +0000

    Task 13

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines add_attribute."""
+
+
+def add_attribute(obj, name, value):
+    """Add a new attribute to an object if possible."""
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
