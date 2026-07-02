7dd5dd80f407c40b345690a4230f1049c5859f48
tala2277 <tt1341700@gmail.com>
2026-07-02 19:16:04 +0000

    Task 14

---
+++ python-input_output/101-stats.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Script that parses logs from stdin and computes metrics."""
+
+import sys
+
+total_size = 0
+line_count = 0
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+
+def print_stats():
+    """Print the collected statistics."""
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code]:
+            print("{}: {}".format(code, status_codes[code]))
+
+
+try:
+    for line in sys.stdin:
+        parts = line.split()
+
+        try:
+            total_size += int(parts[-1])
+            code = parts[-2]
+            if code in status_codes:
+                status_codes[code] += 1
+        except (ValueError, IndexError):
+            pass
+
+        line_count += 1
+        if line_count % 10 == 0:
+            print_stats()
+
+except KeyboardInterrupt:
+    print_stats()
+    raise
+
+print_stats()
