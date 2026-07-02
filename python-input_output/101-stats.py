b5baaa602f1f5c99741a385a4f662cae6886c57e
tala2277 <tt1341700@gmail.com>
2026-07-02 19:19:01 +0000

    Fix Task 14

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -3,47 +3,53 @@
 
 import sys
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
 
-total_size = 0
-line_count = 0
-
-
-def print_stats():
+def print_stats(total_size, status_codes):
     """Print accumulated statistics."""
     print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
-        if status_codes[code] != 0:
+        if status_codes[code]:
             print("{}: {}".format(code, status_codes[code]))
 
 
-try:
-    for line in sys.stdin:
-        line_count += 1
-        parts = line.split()
+def main():
+    """Process log lines from standard input."""
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
 
-        try:
-            total_size += int(parts[-1])
-            code = parts[-2]
-            if code in status_codes:
-                status_codes[code] += 1
-        except (ValueError, IndexError):
-            pass
+    total_size = 0
+    line_count = 0
 
-        if line_count % 10 == 0:
-            print_stats()
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            parts = line.split()
 
-except KeyboardInterrupt:
-    print_stats()
-    raise
+            try:
+                total_size += int(parts[-1])
+                code = parts[-2]
+                if code in status_codes:
+                    status_codes[code] += 1
+            except (ValueError, IndexError):
+                pass
 
-print_stats()
+            if line_count % 10 == 0:
+                print_stats(total_size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(total_size, status_codes)
+        raise
+
+    print_stats(total_size, status_codes)
+
+
+if __name__ == "__main__":
+    main()
