2ada61a5eefd2c013c33d21a1161c2d955d639f2
tala2277 <tt1341700@gmail.com>
2026-07-02 18:51:59 +0000

    Task 0

---
+++ 0-read_file.py
@@ -0,0 +1,9 @@
+cat > 0-read_file.py
+#!/usr/bin/python3
+"""Module that reads a UTF-8 text file and prints its contents."""
+
+
+def read_file(filename=""):
+    """Read a UTF-8 text file and print it to stdout."""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
