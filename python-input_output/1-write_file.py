129c2d1b47f3f94df9bf6cec918058a9365fdf7c
tala2277 <tt1341700@gmail.com>
2026-07-02 18:58:12 +0000

    Fix PEP8 for Task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,9 @@
 
 
 def write_file(filename="", text=""):
-    """Write a string to a text file and return the number of characters written."""
+    """Write a string to a text file.
+
+    Return the number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
