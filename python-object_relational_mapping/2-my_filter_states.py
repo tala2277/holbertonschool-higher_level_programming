2cd42af3a95941adda78c19f8e904c2f68e2154b
tala2277 <tt1341700@gmail.com>
2026-07-24 03:41:12 +0000

    Task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Displays all states matching the provided name."""
+"""Filter states by user input."""
 
 import MySQLdb
 import sys
@@ -8,22 +8,19 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        charset="utf8"
+        port=3306
     )
 
     cur = db.cursor()
-
     cur.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
         .format(sys.argv[4])
     )
 
-    rows = cur.fetchall()
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
 
     cur.close()
