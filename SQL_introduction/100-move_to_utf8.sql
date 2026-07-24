5943a920395663436c60ffc4ba01562ee5f47592
tala2277 <tt1341700@gmail.com>
2026-07-24 02:56:21 +0000

    Fix task 17

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
--- Converts hbtn_0c_0 database, first_table, and name field to UTF8.
+-- Converts hbtn_0c_0 database, first_table and name field to UTF8.
 
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
@@ -8,8 +8,4 @@ USE hbtn_0c_0;
 
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table
-MODIFY name VARCHAR(256)
 COLLATE utf8mb4_unicode_ci;
