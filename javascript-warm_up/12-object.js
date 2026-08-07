6ebac8a513511c35d1c9efab95f130947301ec8e
tala2277 <tt1341700@gmail.com>
2026-08-07 15:15:38 -0700

    Replace object code with SQL query for TV shows
    Removed initial object definition and console logs. Added SQL query to list TV shows without linked genres.

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,11 +1,6 @@
-#!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
-/*
-YOUR CODE HERE
-*/
-myObject.value = 89;
-console.log(myObject);
+-- Lists all shows contained in hbtn_0d_tvshows without a genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
